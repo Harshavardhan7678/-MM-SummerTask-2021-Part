f5b04a13bbed56b31016a04b46cef5c0c43ac9b5
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2021-05-30 11:10:48 +0300

    If applied, this commit will manifest initial Landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './Components/Header';
+
+const useStyles = makeStyles((theme) => ({
+  root:{
+    minHeight: '100vh',
+    backgroundImage:`url(${process.env.PUBLIC_URL + `/Assets/Background.jpeg` })`,
+    backgroundRepeat:"no-repeat",
+    backgroundSize:"cover",
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World.
-      </header>
+    <div className={classes.root}> 
+    <CssBaseline />
+    <Header />
     </div>
   );
 }
